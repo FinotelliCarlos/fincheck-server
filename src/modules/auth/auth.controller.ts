792939deee548ed5ba888a0a5daba4ee9c35b204
finotellicarlos <carlos.finotelli@outlook.com>
2023-06-30 16:24:13 -0300

    feat: config routes public and authorized routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,7 +2,8 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninDto } from './dto/signin.dto';
 import { SignupDto } from './dto/signup.dto';
-
+import { IsPublic } from 'src/shared/decorators/is-public.decorator';
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
